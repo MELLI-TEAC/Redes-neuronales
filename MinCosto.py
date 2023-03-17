7012e39a5ea3bcdf0795c47f87f0287aaa4fe40a
MELLI-TEAC <87356989+MELLI-TEAC@users.noreply.github.com>
2023-03-17 00:35:05 -0500

    Add files via upload

---
+++ MinCosto.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.optimize import least_squares
+
+# Función de activación sigmoide
+def sigmoide(x):
+    return 1 / (1 + np.exp(-x))
+
+# Función de coste
+def funcion_costo(weights, x, y):
+    w11, w12, b1, w21, w22, b2, w31, w32, b3 = weights
+    z1 = sigmoide(w11 * x[1] + w21 * x[2] + b1)
+    z2 = sigmoide(w12 * x[1] + w22 * x[2] + b2)
+    y_esperado = sigmoide(w31 * z1 + w32 * z2 + b3)
+    return (y_esperado - y)**2
+
+# Valores iniciales de los pesos sinápticos y los sesgos
+pesos_inicial = np.array([0.6, 0.3, 0.5, 0.4, 0.8, -0.5, 0.5, -0.4, 0.4])
+
+# Datos de entrada y salida
+x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([0, 1, 1, 0])
+
+# Minimizar la función de coste utilizando el método de Levenberg-Marquardt
+result = least_squares(funcion_costo, pesos_inicial, args=(x.T, y))
+
+# Mostrar los pesos minimizados
+print(f"Pesos minimizados: {result.x}")
